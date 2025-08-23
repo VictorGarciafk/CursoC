7a5e3bda14436f2fde63112dd9966e7d9f3ef083
Victor Hugo <victorhugogamz@gmail.com>
2025-08-22 23:49:24 -0700

    22/08/2025

--- Structs/main.c
+++ Structs/main.c
@@ -1,49 +1,98 @@
 #include<stdio.h>
 #include<string.h>
 
+struct fVencimiento{
+    int dd;
+    int mm;
+    int aa;
+};
+
 struct producto{
     int idProducto;
     char nombre[31];
-    float precio
+    float precio;
+    struct fVencimiento fecha;
 };
 
-void ejercicio1();
+void ingresarProductos();
+struct producto cargaProducto();
+struct fVencimiento cargaFechaVencimiento();
+void imprimir(struct producto p);
+
 
 int main(){
 
-    //ejercicio1();
-
+    ingresarProductos();
     return 0;
 }
 
-void ejercicio1(){
+//Ejercicio 1
+//creamos una variable del tipo struct producto e ingresamos los datos de dos productos(codigo, nombre y precio)
+void ingresarProductos(){
 
-    struct producto prdct[2];
+    struct producto prdct[3];
     int i;
 
-    for (i = 0; i < 2; i++)
+    for (i = 0; i < 3; i++)
     {
         printf("producto #%i\n", i+1);
-
-        printf("ingrese el codigo del producto: ");
-        scanf("%i", &prdct[i].idProducto);
-
-        printf("ingrese el nombre del producto: ");
-        scanf(" %[^\n]", prdct[i].nombre);
-
-        printf("ingrese el precio del producto: ");
-        scanf("%f", &prdct[i].precio);
-
-        printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+        prdct[i] = cargaProducto();
     }
-    
+
     printf("los productos ingresados son los siguientes: ");
-
-    for (i = 0; i < 2; i++)
+    
+    for (i = 0; i < 3; i++)
     {
-        printf("\n#%i-codigo: %i", i, prdct[i].idProducto);
-        printf("\n#%i-nombre: %s", i, prdct[i].nombre);
-        printf("\n#%i-precio: %f", i, prdct[i].precio);
+        imprimir(prdct[i]);
     }
     
+}
+
+
+struct producto cargaProducto()
+{
+    struct producto pr;
+
+    printf("ingrese el codigo del producto: ");
+    scanf("%i", &pr.idProducto);
+
+    printf("ingrese el nombre del producto: ");
+    scanf(" %[^\n]", pr.nombre);
+
+    printf("ingrese el precio del producto: ");
+    scanf("%f", &pr.precio);
+
+    pr.fecha = cargaFechaVencimiento();
+
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+
+    return pr;
+};
+
+struct fVencimiento cargaFechaVencimiento(){
+
+    struct fVencimiento f;
+
+    printf("ingrese el dia de vencimiento del producto: ");
+    scanf("%i", &f.dd);
+
+    printf("ingrese el mes de vencimiento del producto: ");
+    scanf("%i", &f.mm);
+
+    printf("ingrese el año de vencimiento del producto: ");
+    scanf("%i", &f.aa);
+
+    return f;
+}
+
+//al pasar valores unicamente para imprimir no utilizamos los punteros ya que no sobrescribiremos la variable
+//al no pasar con punteros se crea una copia, por ende la variable p es una copia de lo que tenemos en nuestra main
+void imprimir(struct producto p){
+
+    printf("\ncodigo: %i", p.idProducto);
+    printf("\nnombre: %s", p.nombre);
+    printf("\nprecio: %.2f", p.precio);
+    printf("\nFecha de vencimiento: %i/%i/%i", p.fecha.dd, p.fecha.mm, p.fecha.aa);
+    printf("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+
 }
